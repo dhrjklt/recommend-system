5af44fffd6d2f42125d9465f089a22e47de4828c
Dhiraj <dhiraj.kalita@vantagecircle.com>
2019-01-24 23:00:06 +0530

    chnages in json structure

--- production_ready/recomend.py
+++ production_ready/recomend.py
@@ -1,21 +1,35 @@
-
-
-
-from flask import Flask,jsonify,request
+from flask import Flask,request
 import turicreate as tc
-
+import json
 app = Flask(__name__)
 
-model = tc.load_model("popular_deals")
+model1 = tc.load_model("popular_deals")
+model2 = tc.load_model("popular_deals1")
 
+@app.route('/rec/<int:num>',  methods=['GET'])
+def predict(num):
+
+        if request.method == 'GET':
+
+        		rec_list=[]
+                data=model1.recommend([num])
+                for i in range(0,9):
+                	values='exclusivedeal_'+str(data[i]['dealId'])
+                	rec_list.append(values)
+                return json.dumps({'deal':rec_list})
 
 @app.route('/recomend/<int:num>',  methods=['GET'])
 def prediction(num):
 
         if request.method == 'GET':
 
-                return jsonify(list(model.recommend([num])))
+				rec_list=[]
+                data=model2.recommend([num])
+                for i in range(0,9):
+                	values='exclusivedeal_'+str(data[i]['dealId'])
+                	rec_list.append(values)
+                return json.dumps({'deal':rec_list})
 
 
 if __name__ == '__main__':
-	app.run(debug=True,host='0.0.0.0')
+        app.run(debug=True,host='0.0.0.0')
